d99db84d1b31d5e05499812131f793ed5919ad25
abhinavanand-fed <abhinavanand080@gmail.com>
2023-01-17 21:01:16 +0530

    shiftNegToEnd.js

---
+++ shiftNegToEnd.js
@@ -0,0 +1,19 @@
+function shiftNegatives(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+      if (arr[left] < 0) {
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        right--;
+      } else {
+        left++;
+      }
+    }
+    return arr;
+  }
+  let Array = [-5,3,-4,88,-9,-10,21,5,6];
+  console.log(shiftNegatives(Array));
+
+  
