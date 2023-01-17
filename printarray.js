91df5abbfc4896c3bdb9a5ef44b10c9469ffec5e
amShantanu <110715496+amShantanu@users.noreply.github.com>
2023-01-17 23:29:08 +0530

    Create printarray.js
    4

---
+++ printarray.js
@@ -0,0 +1,26 @@
+let arr = [2, -2, 6, 8, 3, -4, 9, -8, -6, -5];
+let x = arr.length;
+arr.sort((a, b) => b - a);
+let pos = [];
+let neg = [];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 0) {
+    pos.push(arr[i]);
+  } else {
+    neg.push(arr[i]);
+  }
+}
+console.log(pos);
+console.log(neg);
+
+let altArr = pos
+  .map(function (s, i) {
+    return [s, neg[i]];
+  })
+  .reduce(function (a, b) {
+    return a.concat(b);
+  });
+
+console.table(altArr);
+//scsucha
