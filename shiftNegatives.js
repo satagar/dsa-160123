f6cd37e30915e3236bee59204c1db41987ce19b2
shubham khatik <khatikshuham1995@gmail.com>
2023-01-19 19:32:19 +0530

    shiftNegatives

---
+++ shiftNegatives.js
@@ -0,0 +1,7 @@
+
+function shiftNegatives(arr) {
+    const nonNegatives = arr.filter(x => x >= 0);
+    const negatives = arr.filter(x => x < 0);
+    return nonNegatives.concat(negatives);
+}
+console.log(shiftNegatives([-5, 3, -4, 88, -9, -10, 21, 5, 6]));
