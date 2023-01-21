06e5bbf1675064e8b3d95b1a528614bde399a74e
yashkr01G <yashkr01@gmail.com>
2023-01-21 16:00:12 +0530

    longest Consecutive Sequence using hashedSet

--- longestConsecutiveSequence.js
+++ longestConsecutiveSequence.js
@@ -1,12 +1,13 @@
 let inputArr=[35,3,4,88,9,10,21,5,6];
 
+//Brute Force Solution
+
 function longConsecSeq(arr){
 
     let maxCount=0
     let currCount=0
 
     arr.sort((a,b)=>a-b);
-    console.log(arr)
     for(let i=0; i<arr.length; i++){
         
         if(arr[i+1]-arr[i]===1){
@@ -22,3 +23,29 @@ function longConsecSeq(arr){
 }
 
 console.log(longConsecSeq(inputArr))
+
+//Optimized Solution using Hashed Set
+
+function longestConsecutiveSequence(arr){
+
+    if(arr==null || arr.length===0) return 0;
+
+    let max=0;
+    const set=new Set(arr);
+
+    for(let num of set){
+        if(set.has(num-1)) continue;
+
+        let currNum=num;
+        let currMax=1;
+
+        while(set.has(currNum+1)){
+            currMax++;
+            currNum++;
+        }
+        max=Math.max(max, currMax);
+    }
+    return max;
+}
+
+console.log(longestConsecutiveSequence(inputArr))
