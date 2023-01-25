1164a9f6f93bbd1535c351e80422eba9c641b5dc
AnkitPayal21 <110723558+AnkitPayal21@users.noreply.github.com>
2023-01-25 20:27:22 +0530

    linear search code

---
+++ LinearSearch.js
@@ -0,0 +1,12 @@
+function LinearSearch(arr,n,key) {
+    for(let i=0;i<n;i++){
+    if(arr[i]==key){
+    return i;
+    }
+    }
+    return -1;
+   }
+   let arr = [2,1,4,6,3,8,5]
+   let n = arr.length
+   
+   console.log(LinearSearch(arr,n,3))
