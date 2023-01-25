7d09e5d6514736f187a7b5d1ce5e14b49d3d2039
boomermitra <110715433+boomermitra@users.noreply.github.com>
2023-01-25 20:46:01 +0530

    multiple

--- linearSearch.js
+++ linearSearch.js
@@ -4,11 +4,10 @@ function linearSearch(arr, item) {
       console.log(`found ${item} at the ${i}th position`);
     }
   }
-  console.log(`item not found`);
 }
 
-let inputArray = [1, 2, 4, 5, 8, 4];
-let item1 = 3;
-let item2 = 4;
+let inputArray = [1, 2, 4, 5, 8, 4, 7, 2];
+let item1 = 2;
+//let item2 = 8;
 linearSearch(inputArray, item1);
-linearSearch(inputArray, item2);
+//linearSearch(inputArray, item2);
