b49b32208532bd831cbe7618fedf8595fc64cf7d
Lokendra Rathore <111693018+lsrathore07@users.noreply.github.com>
2023-01-25 20:31:59 +0530

    Delete linearSearch.js

--- linearSearch.js
+++
@@ -1,8 +0,0 @@
-function linearSearch(arr,k){
-    for(let i=0;i<arr.length;i++){
-          if(arr[i]===k)return i
-    }
-    return "no element found"
-}
-
-console.log(linearSearch([1,2,4,5,6,7],8))
