a003d5005dcc9f52752943a5d2573fc8aa48a19a
Senthil Kumar P <senthilm784@gmail.com>
2023-01-25 20:29:07 +0530

    linear Search

---
+++ LinearSearch.js
@@ -0,0 +1,12 @@
+
+function LinearSearch(arr,target){
+    let result=[]
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]===target){
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+let arr=[33,44,23,5,46,85,4,12,5]
+console.log(LinearSearch(arr, 5))
