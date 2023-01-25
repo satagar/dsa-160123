71841816b35236644f0875fa8080f7d907a62075
Lokendra Singh <lokendrasingh0712@gmail.com>
2023-01-25 20:29:57 +0530

    return idx

--- linearSearch.js
+++ linearSearch.js
@@ -2,6 +2,7 @@ function linearSearch(arr,k){
     for(let i=0;i<arr.length;i++){
           if(arr[i]===k)return i
     }
+    return "no element found"
 }
 
-console.log(linearSearch([1,2,4,5,6,7],6))
+console.log(linearSearch([1,2,4,5,6,7],8))
