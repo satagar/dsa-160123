80bee66083befb1d23236a28f9640f8ea4c4f3ac
suchit <suchitkumar2121@gmail.com>
2023-01-30 22:33:12 +0530

    linear search

--- shiftNegArrToEnd.js
+++ shiftNegArrToEnd.js
@@ -1,7 +1,29 @@
 
 //move all -ve element to end
 
-let arr=[-5,3,-4,88,-9,-10,21,5,6]
+// let arr=[-5,3,-4,88,-9,-10,21,5,6]
+
+
+function shiftNegatives(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+      if (arr[left] < 0) {
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        right--;
+      } else {
+        left++;
+      }
+    }
+    return arr;
+  }
+  let Array = [-5,3,-4,88,-9,-10,21,5,6];
+  console.log(shiftNegatives(Array));
+
+
+/*
 function moveArr(arr,n){
     let temp=new Array(n)
     let j=0;
@@ -26,7 +48,6 @@ if(j==n || j==0) return
 console.log(moveArr(arr,arr.length));
 
 
-/*
 function moveEnd(arr){
     let left=0,right=arr.length-1;
 
