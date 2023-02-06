87d62db7fa625ab8a8b70903d765c4c7428f1f80
uttam rawat <uttamrawat146>
2023-02-06 10:04:52 +0530

    Ternary Search

--- ternarySearch.js
+++ ternarySearch.js
@@ -1,12 +1,12 @@
-function binarySearch(arr, len, num) {
+function ternarySearch(arr, len, num) {
     let start = 0, end = len - 1;
     while (start <= end) {
         let mid1 = parseInt((end - start) / 3) + start;
         let mid2 = end - parseInt((end - start) / 3);
         if (arr[mid1] == num) {
-            return [mid1];
+            return mid1;
         } else if (arr[mid2] == num) {
-            return [mid2];
+            return mid2;
         } else if (arr[mid1] < num && arr[mid2] > mid2) {
             start = mid1 + 1;
             end = mid2 - 1;
@@ -22,7 +22,7 @@ function binarySearch(arr, len, num) {
 
 (function () {
     let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let res = binarySearch(arr, arr.length, 6);
+    let res = ternarySearch(arr, arr.length, 6);
     res === -1 ? console.log('Not Found') : console.log(`Found at index ${res}`);
 })();
 
