636cf3a807b6fafc5fa6a2764f775a859e2d02b4
Senthil Kumar P <senthilm784@gmail.com>
2023-02-08 21:39:46 +0530

    Count Sort

--- BinarySearch.js
+++ BinarySearch.js
@@ -1,18 +1,19 @@
-let arr = [2, 3, 4, 6, 7, 8, 12, 16, 18, 24, 32];
+let arr = [1, 2, 3, 4, 6, 7, 8, 12, 16, 18, 20, 20, 20, 24, 32];
 
 function BinarySearch(arr, target) {
+  let result = [];
   let low = 0;
   let hi = arr.length - 1;
   while (low <= hi) {
-    let mid = Math.floor(low + hi) / 2;
+    let mid = Math.floor((low + hi) / 2);
     if (arr[mid] === target) {
-      return mid;
+      result.push(mid);
     } else if (arr[mid] < target) {
       low = mid + 1;
     } else {
       hi = mid - 1;
     }
   }
-  return -1;
+  return result;
 }
-console.log(BinarySearch(arr, 32));
+// console.log(BinarySearch(arr, 20));
--- ShiftNegToEnd.js
+++ ShiftNegToEnd.js
@@ -1,24 +1,13 @@
-let arr = [22, 4, -8, -10, 5, 6, 72];
+let arr = [-22, 4, -8, -10, 5, -6, -72];
 
 function movenevtolast(arr) {
   let l = 0;
   let r = arr.length - 1;
-  console.log(l);
-  console.log(r);
 
-  for (let i = 0; i < arr.length/2; i++) {
-    if (arr[l] > 0 && arr[r] > 0) {
-      l++;
-    } else if (arr[l] < 0 && arr[r] > 0) {
-      let temp = arr[l];
-      arr[l] = arr[r];
-      arr[r] = temp;
-      l++;
-      r--;
-    } else if (arr[l] < 0 && arr[r] < 0) {
-      r--;
-    } else if (arr[l] > 0 && arr[l] < 0) {
-      l++;
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[l] > 0) l++;
+    else {
+      [arr[l], arr[r]] = [arr[r], arr[l]];
       r--;
     }
   }
