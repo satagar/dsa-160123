bf30ad705be8a5bdb801986d100096767cda405c
shubhamupadhyay09 <110715617+shubhamupadhyay09@users.noreply.github.com>
2023-02-11 22:02:28 +0530

    Update insertAtIdx.js

--- insertAtIdx.js
+++ insertAtIdx.js
@@ -23,8 +23,29 @@ insertAtIdx(index, data) {
         newNode.next = current;
         this.length++
     }
+getElement(index) {
+
+        if (index < 0 || index >= this.length) {
+            console.log("Index out of bound exception");
+            return;
+        }
+
+        let current = this.head;
+        let count = 0;
+
+        while( current !=null){
+            if(count == index){
+                console.log(current.data);
+                break;
+            }
+            count++;
+            current = current.next;
+        }
+    }
 
 l1.insertAtIdx(2, 50);  
-
 l1.printLinkedList();
+console.log("--------------")
+
+l1.getElement(1)
 console.log("--------------")
