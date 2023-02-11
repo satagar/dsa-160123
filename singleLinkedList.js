5e0835452ddd5ec850b0a9f01ecb7fc780d14f83
boomermitra <110715433+boomermitra@users.noreply.github.com>
2023-02-11 20:52:08 +0530

    single linked list

---
+++ singleLinkedList.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class singlyLinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  insertAtBeginning(data) {
+    let newNode = new Node(data);
+    this.size++;
+
+    if (this.head == null) {
+      this.head = newNode;
+      return;
+    }
+
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+}
+
+let myLinkedList = new singlyLinkedList();
+myLinkedList.insertAtBeginning(10);
+myLinkedList.insertAtBeginning(20);
+console.log(myLinkedList.head);
